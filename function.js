a9e3a8905a3917e4d4ef5b7f26ee62f3bc5aea4e
Amit Dubey <amitdubey1826.com>
2023-11-19 22:26:48 +0530

    added function working structure

---
+++ function.js
@@ -0,0 +1,28 @@
+/* 
+js - synchronous singlethreaded lang
+hoisting - accessing the vriable anf functio before those are declared. memory is allocated because of memory allocation phase in global execution content
+scope - local memory where the raviable is accessible
+lexical envirinmeny - local memory + lecical env of parent
+scope chain - chain of lexical environment
+TDZ - phase between variable are being accessed and variable are being assigned value. Between hoisting to initialization.
+Block - group of statements. Example 'if' statement
+
+*/
+
+var x = 1;
+a();
+b();
+console.log(x);
+
+console.log(g);
+let g = 1;
+
+function a() {
+    var x = 10;
+    console.log(x);
+}
+
+function b() {
+    var x = 100;
+    console.log(x);
+}
